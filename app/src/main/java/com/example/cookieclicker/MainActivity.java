ff6493dd15419eb28033d23178901e410bd389ba
brendanjlee <31088155+brendanjlee@users.noreply.github.com>
2019-09-15 02:21:54 -0400

    Edited Variable Names

--- app/src/main/java/com/example/cookieclicker/MainActivity.java
+++ app/src/main/java/com/example/cookieclicker/MainActivity.java
@@ -2,31 +2,37 @@ package com.example.cookieclicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+//This is a test
+
 public class MainActivity extends AppCompatActivity {
 
     private int counter = 0;
-    Button but1;
-    TextView text1;
+    Button count_button;
+    TextView score_text;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        but1 = (Button) findViewById(R.id.button1);
-        text1 = (TextView) findViewById(R.id.tx);
+        count_button = (Button) findViewById(R.id.button1);
+        score_text = (TextView) findViewById(R.id.tx);
 
-        but1.setOnClickListener(new View.OnClickListener() {
+        count_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 counter ++;
-                text1.setText(Integer.toString(counter));
+                score_text.setText(Integer.toString(counter));
             }
         });
     }
