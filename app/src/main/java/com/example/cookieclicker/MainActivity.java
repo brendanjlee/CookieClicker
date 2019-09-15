08de47a2a24836256e23f86a49ed009e75c824a5
Madeline04 <54671985+Madeline04@users.noreply.github.com>
2019-09-15 03:34:44 -0400

    Color Change

--- app/src/main/java/com/example/cookieclicker/MainActivity.java
+++ app/src/main/java/com/example/cookieclicker/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -47,6 +48,14 @@ public class MainActivity extends AppCompatActivity {
 
                 if(counter == 69){
                     score_text.setText("Nice");
+                }
+                if (counter >= 50) {
+                    score_text.setTextColor(Color.parseColor("#b62f13"));
+                    high_text.setTextColor(Color.parseColor("#b62f13"));
+                }
+                else if (counter >= 100) {
+                    score_text.setTextColor(Color.parseColor("#216dc9"));
+                    high_text.setTextColor(Color.parseColor("#216dc9"));
                 }
 
 
